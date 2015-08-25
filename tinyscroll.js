b81d60f18f78c566e7e866254388b8a154798dbc
FrendEr <frend.wong@gmail.com>
2015-08-26 02:10:19 +0800

    udpate

--- tinyscroll.js
+++ tinyscroll.js
@@ -21,8 +21,8 @@
 
     'use strict';
 
-    var CHILD_HEIGHT = 30;
-    var DURATION_TIME = 200;
+    var CHILD_HEIGHT  = 30;     // 30px
+    var DURATION_TIME = 200;    // 200ms
 
     function TinyScroll(options) {
         this.options      = $.extend({}, options);                      // options
@@ -39,8 +39,16 @@
         this.touchY       = 0;                                          // touch start point postion
         this.timeGap      = 0;                                          // time gap
         this.moveState    = 'down';                                     // move state, up or down
-        this.stateTree    = { year: this.options.year || 10000, month: this.options.month || 10000, day: this.options.day || 10000 };    // state tree
-        this.fnList       = { year: this.yearChanged, month: this.monthChanged, day: this.dayChanged };       // function list
+        this.stateTree    = {                                           // state tree
+            year:  this.options.year  || 10000,
+            month: this.options.month || 10000,
+            day:   this.options.day   || 10000
+        };
+        this.fnList       = {                                           // function list
+            year:  this.yearChanged,
+            month: this.monthChanged,
+            day:   this.dayChanged
+        };
 
         this.init();
 
@@ -215,10 +223,14 @@
                 this.$target = $(e.target).hasClass('.ts-item-list') ? $(e.target) : $(e.target).parents('.ts-item-list');
 
                 switch (e.originalEvent.type) {
-                    case 'touchstart' : scope.touchStart.call(scope, e, this.$target); break;
-                    case 'touchmove'  : scope.touchMove.call(scope, e, this.$target);  break;
-                    case 'touchend'   : scope.touchEnd.call(scope, e, this.$target);   break;
-                    default           : break;
+                    case 'touchstart' :
+                        scope.touchStart.call(scope, e, this.$target); break;
+                    case 'touchmove'  :
+                        scope.touchMove.call(scope, e, this.$target);  break;
+                    case 'touchend'   :
+                        scope.touchEnd.call(scope, e, this.$target);   break;
+                    default           :
+                        break;
                 }
             });
         },
@@ -346,13 +358,18 @@
          */
         posTransIndex: function(e, pos) {
             var target = $(e.target).parents('.ts-item-list'),
-                targetId = target.attr('id');
+                targetId = target.attr('id'),
+                dataIndex = target.find('li').eq(2 - (pos / CHILD_HEIGHT)).data('index');
 
             switch (targetId) {
-                case 'year': this.setState({ year: target.find('li').eq(2 - (pos / CHILD_HEIGHT)).data('index') }); break;
-                case 'month': this.setState({ month: target.find('li').eq(2 - (pos / CHILD_HEIGHT)).data('index') }); break;
-                case 'day': this.setState({ day: target.find('li').eq(2 - (pos / CHILD_HEIGHT)).data('index') }); break;
-                default: break;
+                case 'year'  :
+                    this.setState({ year: dataIndex }); break;
+                case 'month' :
+                    this.setState({ month: dataIndex }); break;
+                case 'day'   :
+                    this.setState({ day: dataIndex }); break;
+                default      :
+                    break;
             }
         },
 
