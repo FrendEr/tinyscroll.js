0683fc216435764c34d382be57e9938e4145dabf
kaian.wx <kaian.wx@alibaba-inc.com>
2016-06-23 21:18:03 +0800

    fix #2

--- dist/tinyscroll.js
+++ dist/tinyscroll.js
@@ -237,7 +237,7 @@
             hour = parseInt(hour || this.stateTree.hour);
             minute = parseInt(minute || this.stateTree.minute);
 
-            if (this.time) {
+            if (this.options.time) {
                 return year + '-' +
                     (month  > 9 ? month  : ('0' + month))  + '-' +
                     (day    > 9 ? day    : ('0' + day))    + ' ' +
